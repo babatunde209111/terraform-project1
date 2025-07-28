276e262b8efdd4cdb1bb22bbce9a621fc0b441a9
Ibrahim Salami <sa.babatunde@yahoo.com>
2025-07-28 09:20:21 +0100

    add file

---
+++ variable.tf
@@ -0,0 +1,42 @@
+# vpc Variables
+variable "vpc_cidr" {
+  default     = "10.0.0.0/16"
+  description = "vpc cidr block"
+  type        = string
+}
+# subnet variable 
+variable "public_subnet_az1_cidr" {
+  default     = "10.0.0.0/24"
+  description = "public subnet az1 cidr block"
+  type        = string
+}
+# create public subnet az2/ creating a variable
+variable "public_subnet_az2_cidr" {
+  default     = "10.0.1.0/24"
+  description = "public subnet az2 cidr block"
+  type        = string
+}
+# create private aws az1 variable  here?
+variable "private_app_subnet_az1_cidr" {
+  default     = "10.0.2.0/24"
+  description = "private app subnet az1 cidr block"
+  type        = string
+}
+# create private aws az2 subnet variable
+variable "private_app_subnet_az2_cidr" {
+  default     = "10.0.3.0/24"
+  description = "private app subnet az2 cidr block"
+  type        = string
+}
+# create variable pravite az1 data subnet
+variable "private_data_subnet_az1_cidr" {
+  default     = "10.0.4.0/24"
+  description = "private data subnet az1 cidr block"
+  type        = string
+}
+# create variable pravite az2 data subnet
+variable "private_data_subnet_az2_cidr" {
+  default     = "10.0.5.0/24"
+  description = "private data subnet az2 cidr block"
+  type        = string
+}
