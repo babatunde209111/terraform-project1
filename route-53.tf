2e7bbabc2d0b117f20bdcae3fe2741c45269296d
Ibrahim Salami <sa.babatunde@yahoo.com>
2025-08-10 09:54:13 +0100

    add route53

--- route-53.tf
+++ route-53.tf
@@ -1,19 +1,19 @@
 # # get hosted zone details
 # # terraform aws data hosted zone
 # data "aws_route53_zone" "hosted_zone" {
-#   name = 
+#   name = create a veriable name 
 # }
 
 # # # create a record set in route 53
 # # # terraform aws route 53 record
 # resource "aws_route53_record" "site_domain" {
 #   zone_id = data.aws_route53_zone.hosted_zone.zone_id
-#   name    = 
-#   type    = 
+#   name    = create a veriable 
+#   type    = "A"
 
 #   alias {
-#     name                   = 
-#     zone_id                = 
-#     evaluate_target_health = 
+#     name                   = add the load balanecer dns name
+#     zone_id                = add the load balanecer dns id
+#     evaluate_target_health = true
 #   }
 # }
